a181c1b855b427a0b69c60d89fa1f6a822ac4920
Janemwaura <mwaura461@gmail.com>
2023-10-03 18:56:50 +0300

    finished work

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let numbers = [5, 12, 17, 20, 15];
+var max = Math.max(...numbers);
+console.log(max);
+
+let num3 = 19;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let random = Math.floor(Math.random() * 0.5 ) + 1;
+console.log(random)
+
+
+
+
